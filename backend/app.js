24a233f0f8a6f969f10288a294fb6d5106856eae
kapcode22 <126713176+kapcode22@users.noreply.github.com>
2025-03-05 23:02:36 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const app=express();
 dotenv.config({path:'./config/config.env'});
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://innovate-hub.onrender.com',
     methods:['GET','POST','DELETE','PUT'],
     credentials: true,
 }));
