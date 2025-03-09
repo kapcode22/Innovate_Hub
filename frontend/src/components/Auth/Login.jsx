f496b42f8b90a468a7d5a41df1241ce12f244214
kapcode22 <126713176+kapcode22@users.noreply.github.com>
2025-03-09 13:48:31 +0530

    Update Login.jsx

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
 
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/login",
+        "https://innovate-hub-backend.onrender.com/api/v1/user/login",
         { email, password, role },
         {
           headers: {
